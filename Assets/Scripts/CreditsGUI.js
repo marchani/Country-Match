b3ab3da64b637883b0e38d57f9ca0f4d129324b5
Carlos Marchani <carlos.marchani@gmail.com>
2016-07-27 19:00:13 -0500

    Initial Commit

---
+++ Assets/Scripts/CreditsGUI.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var customSkin : GUISkin;
+var credits : GUIText;
+var buttonW : int;   // button width
+var buttonH : int;    // button height
+var halfScreenW : float;   // half of the Screen width
+var halfButtonW : float;   // half of the button width
+
+function Start ()
+{
+	halfScreenW = Screen.width / 2;
+	halfButtonW = buttonW / 2;
+
+	credits.text = "Designed by\n";
+	credits.text += "Aunt E\n\n";
+	credits.text += "Developed by\n";
+	credits.text += "Carlos\n\n";
+	credits.text += "Tested by\n";
+	credits.text += "Sadie";
+}
+
+function OnGUI ()
+{
+	GUI.skin = customSkin;
+	
+	if(GUI.Button(Rect(halfScreenW - halfButtonW, 1330, buttonW, buttonH), "Return to Menu"))
+	{
+		Application.LoadLevel("title");
+	}
+}
