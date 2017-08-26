2b19c0cf8471c3d7dc86bc6c1de1da3281547989
marchani <carlos.marchani@gmail.com>
2017-08-26 14:28:41 -0500

    Add European Countries (Name)

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -89,7 +89,7 @@ public class GameManager : MonoBehaviour
     //
     public bool LastLevel
     {
-        get { return (currentLevel == 4); }
+        get { return (currentLevel == 6); }
     }
 
 
